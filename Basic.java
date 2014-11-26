8f40d8807301ba7c9b637485a840a4ec0dfdda6c
Joshua Michael Waggoner <rabbitfighter81@gmail.com>
2014-11-26 11:02:37 -0700

    Code overhaul.

--- Basic.java
+++ Basic.java
@@ -1,69 +1,60 @@
-/*  framework to be extended to make desired
-    graphical, event-driven application
-
-    Should 
-
-        make a constructor that calls super and initializes things
-        that should happen before Display is created
-
-    and override
-
-        init:  initialize things that happen after Display is created
-        processInputs:  respond to input events
-        update:  update instance variables to simulate advancing
-                 time and/or response to inputs
-        display:  draw graphics showing current state of things
- */
-
-import java.io.File;
-import java.nio.ByteBuffer;
-
-import javax.imageio.ImageIO;
-
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
-//import org.newdawn.slick.Image;
-
+/**
+ * Framework to be extended to make desired graphical, event-driven application
+ * 
+ * Usage: User should make a constructor that calls super and initializes things
+ * that should happen before Display is created and override the following:
+ * 
+ * - init: initialize things that happen after Display is created -
+ * - processInputs: respond to input events 
+ * - update: update instance variables to
+ *   simulate advancing time and/or response to inputs 
+ * - display: draw graphics
+ *   showing current state of things
+ * 
+ * @author rabbitfighter
+ *
+ */
 public class Basic {
+
 	// test this basic application
-	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		
+
 	}// main
 
-	// instance variables
-	private String title; // title of the application
 	private int frameNumber; // total number of frames displayed
-	private int framesPerSecond; // target fps
+	private int fps; // target fps
 	private int pixelWidth, pixelHeight; // pixel dimension of drawing area
 	private boolean running; // is the application running
 	private boolean closeable; // is the window manually closeable
-	@SuppressWarnings("unused")
 	private boolean resizeable; // is the window manually resizable
 
-	// construct basic application with given title, pixel width and height
-	// of drawing area, and frames per second
-	public Basic(String appTitle, int pw, int ph, int fps) {
-		title = appTitle;
-		frameNumber = 0;
-		framesPerSecond = fps;
-		pixelWidth = pw;
-		pixelHeight = ph;
-		running = false;
-		closeable = true;
-		resizeable = false;
-		
+	/*
+	 * Construct basic application with given title, pixel width and height of
+	 * drawing area, and frames per second
+	 */
+	public Basic(String title, int pw, int ph, int fps) {
+		this.setTitle(title);
+		this.setFrameNumber(0);
+		this.setFps(fps);
+		this.setPixelWidth(pw);
+		this.setPixelHeight(ph);
+		this.setRunning(false);
+		this.setCloseable(true);
+		this.setResizeable(false);
 	}
 
 	public void start() {
-		// create the window
+		/*
+		 * Create the window
+		 */
 		try {
-			Display.setDisplayMode(new DisplayMode(pixelWidth, pixelHeight));
-			Display.setVSyncEnabled(true);
-			Display.create();
 			Display.setTitle(title);
-		
+			Display.setVSyncEnabled(true);
+			Display.setDisplayMode(new DisplayMode(pixelWidth, pixelHeight));
+			Display.create();
 		} catch (LWJGLException e) {
 			System.out.println("Basic constructor failed to create window");
 			e.printStackTrace();
@@ -77,58 +68,31 @@ public class Basic {
 		while (running && (!closeable || !Display.isCloseRequested())) {
 			frameNumber++;
 
-			// these three methods should be overridden by extending
-			// application:
+			/*
+			 * These three methods should be overridden by extending
+			 * application:
+			 */
+			init();
 			processInputs();
 			update();
 			display();
 
 			Display.update();
-			Display.sync(framesPerSecond);
+			Display.sync(fps);
 		}
 
 		Display.destroy();
 
 	}// start
 
-	// ------------------ these methods can be called by the application
-	// --------
-
-	protected int getPixelWidth() {
-		return pixelWidth;
-	}
-
-	protected int getPixelHeight() {
-		return pixelHeight;
-	}
-
-	// report which frame
-	protected int getFrameNumber() {
-		return frameNumber;
-	}
-
-	// report fps (useful for timing in the app)
-	protected int getFPS() {
-		return framesPerSecond;
-	}
-
-	// set the title (perhaps use for successive levels)
-	protected void setTitle(String name) {
-		title = name;
-		Display.setTitle(title);
-	}
-
-	// set whether the window can be closed manually
-	protected void setCloseable(boolean value) {
-		closeable = value;
-	}
-
 	// halt the application under program control
 	protected void halt() {
 		running = false;
 	}
 
-	// ------------------ these methods should be overridden ------------------
+	/*
+	 * Methods to be overridden
+	 */
 
 	protected void init() {
 	}
@@ -142,4 +106,73 @@ public class Basic {
 	protected void display() {
 	}
 
-}// Basic
+	/***** Getters/Setters *****/
+
+	// instance variables
+	private String title; // title of the application
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getFrameNumber() {
+		return frameNumber;
+	}
+
+	public void setFrameNumber(int frameNumber) {
+		this.frameNumber = frameNumber;
+	}
+
+	public int getFps() {
+		return fps;
+	}
+
+	public void setFps(int fps) {
+		this.fps = fps;
+	}
+
+	public int getPixelWidth() {
+		return pixelWidth;
+	}
+
+	public void setPixelWidth(int pixelWidth) {
+		this.pixelWidth = pixelWidth;
+	}
+
+	public int getPixelHeight() {
+		return pixelHeight;
+	}
+
+	public void setPixelHeight(int pixelHeight) {
+		this.pixelHeight = pixelHeight;
+	}
+
+	public boolean isRunning() {
+		return running;
+	}
+
+	public void setRunning(boolean running) {
+		this.running = running;
+	}
+
+	public boolean isCloseable() {
+		return closeable;
+	}
+
+	public void setCloseable(boolean closeable) {
+		this.closeable = closeable;
+	}
+
+	public boolean isResizeable() {
+		return resizeable;
+	}
+
+	public void setResizeable(boolean resizeable) {
+		this.resizeable = resizeable;
+	}
+
+}// EOF
